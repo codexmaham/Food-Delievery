43d1a69a8e74b2e6497981f2e568d35cf18967f1
Maham Fatima <codexmaham@gmail.com>
2025-09-24 23:10:33 +0500

    Create Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
 
 const App = () => {
   return (
     <div className='app'>
     <Navbar/>
+    <Routes>
+      <Route path='' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+\
+      <Route path='' element={<Home/>}/>
+    </Routes>
       
     </div>
   )
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>OH HUNGRY..?</h2>
+            <p>Order something healthy, don't feel regret just enjoy the calories</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
