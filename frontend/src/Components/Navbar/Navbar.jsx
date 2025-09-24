1174e1987573528c9d4913f227fafc284649db7d
Maham Fatima <codexmaham@gmail.com>
2025-09-24 16:05:22 +0500

    First Commit

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+  
+  return (
+    <div className="navbar">
+      <img src={assets.Logo} alt="" className="logo" />
+      <ui className="navbar-menu">
+        <li className={menu === "home" ? "active" : "hh"}>Home</li>
+        <li className={menu === "menu" ? "active" : ""}>Menu</li>
+        <li className={menu === "careers" ? "active" : ""}>Careers</li>
+        <li className={menu === "contact" ? "active" : ""}>Contact</li>
+      </ui>
+      <div className="navbar-right">
+        <img
+          src={assets.searchicon}
+          alt="icon"
+          style={{ width: "40px", aspectRatio: "1 / 1", objectFit: "contain" }}
+        />
+        <div className="navbar-search">
+          <img
+            src={assets.basketicon}
+            alt="icon"
+            style={{
+              width: "40px",
+              aspectRatio: "1 / 1",
+              objectFit: "contain",
+            }}
+          />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,32 @@
+import Logo from "./Logo.png"
+import Food_1 from "./Food_1.jpg"
+import Food_2 from "./Food_2.jpg"
+import Food_3 from "./Food_3.jpg"
+import Food_4 from "./Food_4.jpg"
+import Food_5 from "./Food_5.jpg"
+import Food_6 from "./Food_6.jpg"
+import Food_7 from "./Food_7.jpg"
+import Food_8 from "./Food_8.jpg"
+import Food_9 from "./Food_9.jpg"
+import Food_10 from "./Food_10.jpg"
+import searchicon from "./search.svg"
+import basketicon from "./basket.svg"
+
+
+
+export const assets={
+    basketicon,
+    searchicon,
+    Logo,
+    Food_1,
+Food_2,
+Food_3,
+Food_4,
+Food_5,
+Food_6,
+Food_7,
+Food_8,
+Food_9,
+Food_10,
+   
+}
