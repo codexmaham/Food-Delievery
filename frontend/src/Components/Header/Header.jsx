69dc06b470a4ef6be50beecdcc6d711722517f9e
Maham Fatima <codexmaham@gmail.com>
2025-09-25 17:20:15 +0500

    update header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Header.css'
+import LiquidEther from '../../Background/LiquidEther'
 const Header = () => {
   return (
     <div className="header">
+
+   
         <div className="header-contents">
-            <h2>OH HUNGRY..?</h2>
-            <p>Order something healthy, don't feel regret just enjoy the calories</p>
-            <button>View Menu</button>
+          <h2>OH HUNGRY..?</h2>
+          <p>
+            Order something healthy, don't feel regret just enjoy the calories
+          </p>
+          <button>View Menu</button>
         </div>
+      
     </div>
-  )
+  );
 }
 
 export default Header
